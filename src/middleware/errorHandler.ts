68fc56652f538da3f3f8496bb3f38d67fdae9b08
Souvik Das <dev.souvik2002@gmail.com>
2024-11-27 10:14:09 +0530

    remove error stack trace

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -3,5 +3,5 @@ import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
-  res.status(err.status || 500).json([{message: err.stack}]);
+  res.status(err.status || 500).json([{message: err.message}]);
 };
--- src/util/wraAsyncRoutes.ts
+++ src/util/wraAsyncRoutes.ts
@@ -2,15 +2,10 @@ import { Router } from "express";
 import { asyncErrorHandler } from "./asyncErrorHandler";
 
 export const wrapAsyncRoutes = (router: Router) => {
-    // console.log(router.stack)
   router.stack.forEach((layer: any) => {
     if (layer.route) {
       layer.route.stack.forEach((routeHandler: any) => {
-        // console.log(routeHandler.handle);
-        
         routeHandler.handle = asyncErrorHandler(routeHandler.handle);
-        // console.log(routeHandler.handle);
-        
       });
     }
   });
